1fe6e8369c0d23b25bfc8aa674a5322fdb0db837
Ahmed Habib <66479808+ahmedhabiib@users.noreply.github.com>
2021-08-23 10:45:14 +0300

    added my custom user input hook challenge

--- src/components/BasicForm.js
+++ src/components/BasicForm.js
@@ -1,25 +1,89 @@
+import useInput from '../hooks/Input/use-input';
+
 const BasicForm = (props) => {
-  return (
-    <form>
-      <div className='control-group'>
-        <div className='form-control'>
-          <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
-        </div>
-        <div className='form-control'>
-          <label htmlFor='name'>Last Name</label>
-          <input type='text' id='name' />
-        </div>
-      </div>
-      <div className='form-control'>
-        <label htmlFor='name'>E-Mail Address</label>
-        <input type='text' id='name' />
-      </div>
-      <div className='form-actions'>
-        <button>Submit</button>
-      </div>
-    </form>
-  );
+	const {
+		value: emailValue,
+		isValid: emailIsValid,
+		hassError: emailIsInValid,
+		valueBlurHandler: emailBlur,
+		valueChangeHandler: emailChangeHandler,
+		reset: emailReset
+	} = useInput((value) => value.includes('@'));
+
+	const {
+		value: firstnameValue,
+		valueBlurHandler: firstnameBlur,
+		hassError: firstnameIsInValid,
+		valueChangeHandler: firstnameChangeHandler,
+		isValid: firstnameIsValid,
+		reset: firstnameReset
+	} = useInput((value) => value.trim() !== '');
+
+	const {
+		value: lastnameValue,
+		valueBlurHandler: lastnameBlur,
+		hassError: lastnameIsInValid,
+		valueChangeHandler: lastnameChangeHandler,
+		isValid: lastnameIsValid,
+		reset: lastnameReset
+	} = useInput((value) => value.trim() !== '');
+
+	let formIsValid = false;
+
+	if (emailIsValid && firstnameIsValid && lastnameIsValid) {
+		formIsValid = true;
+	}
+
+	const submitFormHandler = (e) => {
+		e.preventDefault();
+		if (firstnameIsInValid && lastnameIsInValid && emailIsInValid) {
+			return;
+		}
+		emailReset();
+		firstnameReset();
+		lastnameReset();
+	};
+
+	const firstnameClasses = firstnameIsInValid ? 'form-control invalid' : 'form-control';
+	const lastnameClasses = lastnameIsInValid ? 'form-control invalid' : 'form-control';
+	const emailClasses = emailIsInValid ? 'form-control invalid' : 'form-control';
+
+	return (
+		<form onSubmit={submitFormHandler}>
+			<div className="control-group">
+				<div className={firstnameClasses}>
+					<label htmlFor="name">First Name</label>
+					<input
+						type="text"
+						id="name"
+						value={firstnameValue}
+						onBlur={firstnameBlur}
+						onChange={firstnameChangeHandler}
+					/>
+					{firstnameIsInValid && <p className="error-text"> Pleased enter your firstname!</p>}
+				</div>
+				<div className={lastnameClasses}>
+					<label htmlFor="lastname">Last Name</label>
+					<input
+						type="text"
+						id="lastname"
+						value={lastnameValue}
+						onBlur={lastnameBlur}
+						onChange={lastnameChangeHandler}
+					/>
+					{lastnameIsInValid && <p className="error-text"> Pleased enter your lastname!</p>}
+				</div>
+			</div>
+			<div className={emailClasses}>
+				<label htmlFor="email">E-Mail Address</label>
+				<input type="email" id="email" value={emailValue} onBlur={emailBlur} onChange={emailChangeHandler} />
+				{emailIsInValid && <p className="error-text">Please enter valid email!</p>}
+			</div>
+			<div className="form-actions">
+				<button disabled={!formIsValid}>Submit</button>
+			</div>
+		</form>
+	);
 };
 
 export default BasicForm;
--- src/hooks/Input/use-input.js
+++ src/hooks/Input/use-input.js
@@ -5,13 +5,13 @@ const useInput = (validateValue) => {
 	const [ isTouched, setIsTouched ] = useState(false);
 
 	const valueIsValid = validateValue(enteredValue);
-	const hasError = !valueIsValid && isTouched;
+	const hassError = !valueIsValid && isTouched;
 
 	const valueChangeHandler = (e) => {
 		setEnteredValue(e.target.value);
 	};
 
-	const inputBlurHandler = (e) => {
+	const valueBlurHandler = () => {
 		setIsTouched(true);
 	};
 
@@ -22,11 +22,11 @@ const useInput = (validateValue) => {
 
 	return {
 		value: enteredValue,
-		isValid: valueIsValid,
-		hasError,
-		reset,
+		valueBlurHandler,
+		hassError,
 		valueChangeHandler,
-		inputBlurHandler
+		isValid: valueIsValid,
+		reset
 	};
 };
 
